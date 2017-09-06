e0514637fd9705aa615341c84607d6d564735984
mazurevich <andrey_mazurevich@epam.com>
2017-09-06 13:32:07 +0300

    some preparation for SSR

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 import ReactDom from 'react-dom'
 import React from 'react'
-import App from './components/App'
+import App from '../shared/App'
 
 const app = document.getElementById('app');
 ReactDom.render(<App/>, app)
